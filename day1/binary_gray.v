fe131aaf1174e1db266f43eb70b50d82f7ca8e1a
Suganthi1218 <suganthi1218@gmail.com>
2024-06-25 18:35:10 +0530

    Create binary_gray.v

---
+++ day1/binary_gray.v
@@ -0,0 +1,26 @@
+module binary_gray(input [3:0]binary, output [3:0] gray);
+
+assign gray[3]=binary[3];
+assign gray[2]=binary[3]^binary[2];
+assign gray[1]=binary[2]^binary[1];
+assign gray[0]=binary[1]^binary[0];
+endmodule
+
+module binary_gray_tb;
+
+reg[3:0]  binary;
+wire [3:0] gray;
+
+binary_gray uut(binary,gray);
+
+initial 
+begin 
+
+binary=4'b0011; #10
+binary=4'b0001; #10
+binary=4'b0111; #10
+binary=4'b1011; #10
+binary=4'b1010; 
+
+end 
+endmodule
